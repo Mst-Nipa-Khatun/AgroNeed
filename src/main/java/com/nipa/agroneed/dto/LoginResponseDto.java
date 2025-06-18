2102bc7a10e4f94c912e4c2eff03da8f01f6930a
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-06-18 12:07:37 +0600

    LoginDto

---
+++ src/main/java/com/nipa/agroneed/dto/LoginResponseDto.java
@@ -0,0 +1,13 @@
+package com.nipa.agroneed.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class LoginResponseDto {
+    private Long userId;
+    private String username;
+    private List<String> roles;
+    private String token;
+}
