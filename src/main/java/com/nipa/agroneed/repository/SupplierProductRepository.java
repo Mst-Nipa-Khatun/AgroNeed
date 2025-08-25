3e6b41dccfc6f74507083c053631a2100b5bd000
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-08-25 14:16:53 +0600

    SupplierProductRepository and SupplierRepository added

---
+++ src/main/java/com/nipa/agroneed/repository/SupplierProductRepository.java
@@ -0,0 +1,9 @@
+package com.nipa.agroneed.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierProductRepository extends JpaRepository<SupplierProductRepository,Long> {
+
+}
---
+++ src/main/java/com/nipa/agroneed/repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.nipa.agroneed.repository;
+
+import com.nipa.agroneed.entity.SupplierEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<SupplierEntity, Long> {
+}
